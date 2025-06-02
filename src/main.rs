286ead3f70651b4ec5893df21e4355088ef001d6
KanataIkumi <i.kanata0531@gmail.com>
2025-06-03 00:26:17 +0900

    ログファイルの内容を読み込み、ゼロ除算のエラーハンドリングを追加しました。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{fs, io::Error};
+
 fn main() {
-    println!("Hello, world!");
+    let file_content = fs::read_to_string("logs.txt");
+    println!("{:#?}", file_content);
+
+    let result = divide(10.0, 0.0);
+    println!("{:#?}", result);
+
+    let result = divide(10.0, 2.0);
+    println!("{:#?}", result);
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        return Err(Error::other("Cannot divide by zero".to_string()));
+    }
+    Ok(a / b)
 }
